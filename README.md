a3d57e6975cbea2bd082e4e8094efc59f3c3ece0
KWAHK SEUNGWOO <rhkr827@gmail.com>
2022-08-14 15:33:32 +0900

    Commit 20220814

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
-[English Version](README_ENG.md)
-
 # MyMail
+    - Mail 자동 분류 및 정리 위한 프로젝트  
+#### 프로젝트 목적
+    - Mail 자동 분류 및 정리 위한 프로젝트
+    - Gmail 정리하다, 메일 리스트화하여 정리하는 것이 나을 것 같아 계획
+#### 프로젝트 계획
+##### Ver 1.0 Google Gmail Filter 생성을 위한 Python Scripts
+    - 개인 Gmail에 있는 모든 메일의 보낸 사람 메일 주소를 저장 후 그룹화
+    - 그룹화된 메일 주소 중 필요한 것은 Gmail Filter 생성, 필요 없는 거는 메일 오는 것을 막거나 삭제
 
-## Google Gmail Filter 생성을 위한 Python Scripts
-
-- 귀차니즘으로 인해 제작 진행 중
-- 개인 Gmail에 있는 모든 메일의 보낸 사람 메일 주소를 저장 후 그룹화
-- 그룹화된 메일 주소 중 필요한 것은 Gmail Filter 생성, 필요 없는 거는 메일 오는 것을 막거나 삭제
-- 장기적으로 Kakao, Naver, Nate mail 등도 비슷한 방법으로 메일 정리 용도로 사용 예정
-  - 시간이 된다면, GUI(아마도 PyQT)로 제작하여 필터 또는 자동 분류가 가능하도록 하게도 만들 계획
-
-## 개발 환경
-- OS : Windows   
-- IDE : Visual Studio Code
-- Function : Python
-- API : Google API OAuth, Gmail API
+##### Ver 2.0 Outlook으로 메일 List 생성 Python scripts
+    - Office 365 구독한 김에 Outlook에 타 메일 계정 연동하여 메일 List 생성
+    - Ver 1.0 Refactoring하여 기존 Gmail mail별, Domain별 List 생성 코드와 구분하여 진행
+    - Python으로 domain > mail 구조로 규칙 생성 후 정리
--- modules/auth.py
+++ modules/auth.py
@@ -1,3 +1,4 @@
+import json
 import os
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -7,16 +8,25 @@ from google.oauth2.credentials import Credentials
 
 def gmail_authenticate():
     SCOPES = ['https://mail.google.com/']
+    SETTING_JSON = 'settings.json'
     creds = None
-    if os.path.exists('../MyGoogleOAuth/token.json'):
-        creds = Credentials.from_authorized_user_file('../MyGoogleOAuth/token.json', SCOPES)
+
+    settings = {}
+    if os.path.exists(SETTING_JSON):
+        with open('./settings.json', 'r') as f:
+            settings = json.load(f)
+
+    auth_path = str(settings['google_auth_path'])
+
+    if os.path.exists(auth_path + 'token.json'):
+        creds = Credentials.from_authorized_user_file(auth_path + 'token.json', SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file('../MyGoogleOAuth/token.json', SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file(auth_path + 'token.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        with open('../MyGoogleOAuth/token.json', 'w') as token:
+        with open(auth_path + 'token.json', 'w') as token:
             token.write(creds.to_json())
 
     return build('gmail', 'v1', credentials=creds)
